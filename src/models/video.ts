367289e2a79df43aa85698ff5a6711a9f17898ff
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2025-08-28 22:14:47 -0300

    Criação de video.ts

---
+++ src/models/video.ts
@@ -0,0 +1,32 @@
+/**
+ *  programa: video.ts
+ *  autor: Gabriel da Silva Freitas
+ *  data: 28/08/2025
+ *  descrição: arquivo contendo classe abstrata responsável pela modelagem das informações do vídeo e suas funções
+ * e as interfaces do vídeo
+ */
+
+export abstract class Video{
+    constructor(
+        public id: number,
+        public titulo: string,
+        private duracaoEmSegundos: number,
+        public autor: string,
+        public views: number
+    ){}
+
+    abstract play(): void;
+    abstract pause(): void;
+    abstract stop(): void;
+    abstract info(): string;
+}
+
+export interface Playable extends Video{
+    play(): void;
+    pause(): void;
+    stop(): void;
+}
+
+export interface Describable extends Video{
+    info(): string;
+}
